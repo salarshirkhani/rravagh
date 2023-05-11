74738767718220231ee34a727c936f93bbf295d6
salarshirkhani <salarshirkhany16@gmail.com>
2023-05-11 13:03:18 +0330

    change config

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,7 +45,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => '/home/rravagh/public_html/ravaghh/pics',
         ],
 
         'public' => [
